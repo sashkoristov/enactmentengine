fb4147ef88930ef28d22771140d9ce3c35a06d4f
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2019-11-21 15:55:18 +0100

    refactored code, added some comments

--- src/main/java/at/enactmentengine/serverless/main/OpenWhiskHandler.java
+++ src/main/java/at/enactmentengine/serverless/main/OpenWhiskHandler.java
@@ -6,6 +6,7 @@ import at.enactmentengine.serverless.parser.YAMLParser;
 import com.cloudant.client.api.ClientBuilder;
 import com.cloudant.client.api.CloudantClient;
 import com.cloudant.client.api.Database;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.slf4j.Logger;
@@ -26,56 +27,69 @@ import java.util.Properties;
  */
 public class OpenWhiskHandler {
 
-    @SuppressWarnings("unchecked")
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static JsonObject main(JsonObject args) {
-        final Logger logger = LoggerFactory.getLogger(App.class);
-        YAMLParser yamlParser = new YAMLParser();
+        long startTime = System.currentTimeMillis();
+
+        // Disable hostname verification (enable OpenWhisk connections)
         final Properties props = System.getProperties();
         props.setProperty("jdk.internal.httpclient.disableHostnameVerification", Boolean.TRUE.toString());
 
+        // Get input filename and possible additional parameters
         String filename = null;
         if (args != null && args.has("filename"))
             filename = args.getAsJsonPrimitive("filename").getAsString();
-        Map<String, Object> jsonMap = new HashMap<>();
+        HashMap jsonMap = new HashMap<>();
         if (args != null && args.has("params"))
             jsonMap = new Gson().fromJson(args.get("params").toString(), HashMap.class);
 
+        // Create result object
         JsonObject response = new JsonObject();
-        String in;
 
-        if (filename != null) {
-            InputStream tmp = getFileFromCloudant(filename);
-            in = "test";
-        } else {
-            response.addProperty("result", "No filename defined!");
+        // Check if filename is specified
+        if (filename == null) {
+            response.addProperty("result", "Error: No filename specified.");
             return response;
         }
 
-        System.out.println(in + " ");
-
-        logger.debug("Parsing file " + filename + "!");
-        ExecutableWorkflow ex = yamlParser.parseExecutableWorkflow(in);
+        // Parse and create executable workflow
+        ExecutableWorkflow ex = new YAMLParser().parseExecutableWorkflow(filename);
         if (ex != null) {
-            Map<String, Object> input = new HashMap<String, Object>();
 
+            // Set the workflow input
+            Map<String, Object> input = new HashMap<String, Object>();
             input.put("some source", "5");// for ref gate
             input.put("some camera source", "0");
             input.put("some sensor source", "0");
+
+            // Add params from EE call as input
             input.putAll(jsonMap);
+
+            // Execute workflow
             try {
                 ex.executeWorkflow(input);
             } catch (MissingInputDataException e) {
                 logger.error(e.getMessage(), e);
-                response.addProperty("result", "Workflow stopped with errors! See Log for details!");
+                response.addProperty("result", "Error: Could not run workflow. See logs for more details.");
                 return response;
             }
         }
 
-        response.addProperty("result", "Workflow ran without errors! See logs for details!");
+        long endTime = System.currentTimeMillis();
+        response.addProperty("result", "Workflow ran without errors in " + (endTime - startTime) + "ms. Start: "+ startTime +", End: " + endTime);
         return response;
     }
 
+    /**
+     * Get a file from cloudant
+     *
+     * @param docId id of the document
+     * @return InputStream of the file
+     */
     private static InputStream getFileFromCloudant(String docId) {
+
+        // Authenticate
         CloudantClient client = null;
         try {
             Properties properties = new Properties();
@@ -89,15 +103,13 @@ public class OpenWhiskHandler {
             throw new RuntimeException("Client not created", e);
         }
 
-        System.out.println(client);
+        // Database access
         Database db = null;
         try {
             db = client.database("input_files", false);
         } catch (Exception e) {
             throw new RuntimeException("DB Not found", e);
         }
-        System.out.println(db);
-
         InputStream is = db.getAttachment("yaml_files", docId);
 
         return is;
