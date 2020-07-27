97a2a06f13111f7e115254455ecf58e40f5e7049
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2020-07-27 13:21:34 +0200

    refactored handler and added interaction with logger service

--- src/main/java/at/enactmentengine/serverless/main/Handler.java
+++ src/main/java/at/enactmentengine/serverless/main/Handler.java
@@ -1,49 +1,61 @@
 package at.enactmentengine.serverless.main;
 
 import at.enactmentengine.serverless.nodes.FunctionNode;
-import at.enactmentengine.serverless.object.FunctionInvocation;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
-import java.io.DataOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Class to handle service requests.
+ *
+ * @author stefanpedratscher
+ */
 public class Handler implements Runnable {
 
+    /**
+     * Logger for request handler.
+     */
     private final static Logger LOGGER = Logger.getLogger(Handler.class.getName());
 
+    /**
+     * Connected client sending the request.
+     */
     private Socket socket;
 
+    /**
+     * Default constructor for handler.
+     *
+     * @param socket client connecting to service.
+     */
     public Handler(Socket socket) {
         this.socket = socket;
     }
 
+    /**
+     * Handle request.
+     */
     @Override
     public void run() {
 
+        /* Get input and output stream */
         InputStream in = null;
         OutputStream out = null;
-
         try {
             in = socket.getInputStream();
+            out = new FileOutputStream(Thread.currentThread().getId() + ".yaml");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
-        try {
-            out = new FileOutputStream(Thread.currentThread().getId() + ".yaml");
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
-
+        /* Read bytes and write to output stream */
         byte[] bytes = new byte[16 * 1024];
         int count;
         try {
@@ -61,14 +73,39 @@ public class Handler implements Runnable {
             e.printStackTrace();
         }
 
-        Executor executor = new Executor();
-        Map<String, Object> result = new HashMap<>();
-        result.put("wfResult", executor.executeWorkflow(Thread.currentThread().getId() + ".yaml"));
-        result.put("fInvocations", FunctionNode.functionInvocations);
+        /* Start measuring time for workflow execution */
+        Long start = System.currentTimeMillis();
 
+        /* Get the execution id of the workflow execution */
+        FunctionNode.executionId = getExecutionId();
+
+        /* Execute the workflow */
+        Executor executor = new Executor();
+        Map<String, Object> executionResult = executor.executeWorkflow(Thread.currentThread().getId() + ".yaml");
+
+        /* Stop measuring time for workflow execution */
+        Long end = System.currentTimeMillis();
+
+        /* Prepare the execution result */
+        Map<String, Object> result = new HashMap<>();
+
+        /* Workflow result */
+        result.put("wfResult", executionResult);
+
+        /* Workflow execution identifier */
+        result.put("executionId", FunctionNode.executionId);
+
+        /* Enactment engine statistics */
+        Map<String, Object> eeStats = new HashMap<>();
+        eeStats.put("start", new Timestamp(start + TimeZone.getTimeZone("Europe/Rome").getOffset(start)).toString());
+        eeStats.put("end", new Timestamp(end + TimeZone.getTimeZone("Europe/Rome").getOffset(start)).toString());
+        result.put("eeStats", eeStats);
+
+        /* Send back json string because other modules might not have GSON */
         String jsonResult = new Gson().toJson(result);
         LOGGER.log(Level.INFO, "Sending back result " + jsonResult);
 
+        /* Send response back to client */
         DataOutputStream dOut;
         try {
             dOut = new DataOutputStream(socket.getOutputStream());
@@ -78,12 +115,60 @@ public class Handler implements Runnable {
             e1.printStackTrace();
         }
 
+        /* Close connection */
         try {
             in.close();
             assert out != null;
             out.close();
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get the execution identifier from the logger service
+     *
+     * @return execution identifier
+     */
+    private int getExecutionId(){
+        try {
+            /* Connect to logger service */
+            LOGGER.info("Connecting to logger service...");
+            Socket loggerService = new Socket("logger-service", 9005);
+
+            /* Prepare and send request */
+            JsonObject request = new JsonObject();
+            request.addProperty("requestType", "GET_EXECUTION_ID");
+            String requestString = request.toString();
+            LOGGER.info("Sending request " + requestString + "...");
+            ObjectOutputStream out = new ObjectOutputStream(loggerService.getOutputStream());
+            out.writeObject(requestString);
+            out.flush();
+
+            /* Wait for response (wait for filtered resources) */
+            LOGGER.info("Waiting for response from logger service...");
+            ObjectInputStream objectInputStream = new ObjectInputStream(loggerService.getInputStream());
+            String result = (String) objectInputStream.readObject();
+
+            /* Close connection */
+            LOGGER.info("Closing connection to logger service...");
+            loggerService.close();
+
+            /* Check if logger service returned a valid execution identifier */
+            if(result == null || (result != null && "-1".equals(result))){
+                LOGGER.warning("Logger service returned an invalid executionId.");
+                return -1;
+            }
+
+            /* Return response */
+            LOGGER.info("Response got: " + result);
+            return Integer.parseInt(result);
+        } catch (IOException | ClassNotFoundException e) {
+
+            /* Log error on failure */
+            LOGGER.severe("Could not get execution Id: " + e.getLocalizedMessage());
+            return -1;
         }
     }
 }
