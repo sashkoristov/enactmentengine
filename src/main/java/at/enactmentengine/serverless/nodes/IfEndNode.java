6c4deaca31b1c2daaac0c49a5047fb362bc3067f
noeckl <noeckl96@gmail.com>
2020-12-21 13:17:11 +0100

    Removed unused inport

--- src/main/java/at/enactmentengine/serverless/nodes/IfEndNode.java
+++ src/main/java/at/enactmentengine/serverless/nodes/IfEndNode.java
@@ -4,7 +4,6 @@ import at.uibk.dps.afcl.functions.objects.DataOuts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
