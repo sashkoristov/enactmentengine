7db930f5d5a05e254dc66e4c488733972caf6b0c
Stefan Kotrba <stefan.kotrba@uibk.students.ac.at>
2021-08-01 12:47:51 +0200

    add class async-handler

--- src/main/java/at/enactmentengine/serverless/Handler/AsyncHandler.java
+++ src/main/java/at/enactmentengine/serverless/Handler/AsyncHandler.java
@@ -1,4 +1,210 @@
 package at.enactmentengine.serverless.Handler;
 
-public class AsyncHandler {
+import at.uibk.dps.afcl.functions.objects.DataIns;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.*;
+
+public class AsyncHandler{
+
+    private boolean isAsync;
+    private List<DataIns> functions;
+
+    private ArrayList<String> running;
+    private ArrayList<String> failed;
+    private ArrayList<String> finished;
+    private long time;
+
+    public AsyncHandler(boolean isAsync, List<DataIns> functions)
+    {
+        this.isAsync = isAsync;
+        this.functions = functions;
+        this.failed = new ArrayList<>();
+
+        this.running = new ArrayList<>();
+        this.finished = new ArrayList<>();
+    }
+
+    public void run() {
+
+        for (DataIns function: this.functions) {
+            try {
+                this.handle(function.getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handle(String function) throws IOException {
+        String queryId = makeLogQuery(function);
+        String returnString = retrieveLogData(queryId);
+        checkFunction(returnString,function);
+    }
+
+    /**
+     * checks if a function is running, failed or finished
+     *
+     * @param returnString the log data for the function
+     * @param function the function we check
+     */
+    private void checkFunction(String returnString,String function){
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = new JSONObject();
+        try {
+            //convert the string to a json
+            jsonObject = (JSONObject) jsonParser.parse(returnString);
+            //get the query results out of the return string
+            JSONArray results = (JSONArray) jsonObject.get("results");
+
+            //iterates over rows from log
+            outerLoop:
+            for (Object lastResult: results) {
+                JSONObject[] array = (JSONObject[]) lastResult;
+                //iterates over items in row
+                for (JSONObject object : array) {
+                    if (object.get("field") == "@message" && ((String) object.get("value")).startsWith("START")) {
+                        this.running.add(function);
+                        break outerLoop;
+                    } else if (object.get("field") == "@message" && ((String) object.get("value")).startsWith("END")) {
+                        //iterates over rows in log
+                        for (Object entries : results) {
+                            //iterates over items in row
+                            for (JSONObject entry : (JSONObject[]) entries) {
+                                if (object.get("field") == "@message" && ((String) object.get("value")).startsWith("[ERROR]")) {
+                                    this.failed.add(function);
+                                    break outerLoop;
+                                }
+                                if (object.get("field") == "@message" && ((String) object.get("value")).startsWith("START")) {
+                                    this.finished.add(function);
+                                    break outerLoop;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            //print exception
+            System.out.println("Exception:\n" + e);
+        }
+    }
+
+    /**
+     * retrieves the data from a query
+     *
+     * @param queryId the query id for the query we want the data
+     *
+     * @return the data in string form
+     */
+    private String retrieveLogData(String queryId){
+        //assample the terminal command
+        String command = "aws logs get-query-results --query-id " + queryId;
+        String[] exec = new String[]{"/bin/bash", "-c", command};
+
+        String returnString = "";
+        try {
+            Process proc = Runtime.getRuntime().exec(exec);
+
+            // Read the output from the command
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(proc.getInputStream()));
+            System.out.println("Here is the standard output of the command:\n");
+            String s = null;
+
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+                returnString += s;
+            }
+            // Read any errors from the attempted command
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(proc.getErrorStream()));
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (Exception e) {
+            //print exception
+            System.out.println("Exception:\n" + e);
+        }
+        return returnString;
+    }
+
+    /**
+     * makes a log query for the function
+     *
+     * @param function the function we want the logs for
+     *
+     * @return the query id from the executed query
+     */
+    private String makeLogQuery(String function) {
+        //create timestamp for start and end time
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+        calendar.clear();
+        LocalTime.now(TimeZone.getDefault().toZoneId());
+        calendar.set(LocalDate.now().getYear(),LocalDate.now().getMonth().getValue()-1,LocalDate.now().getDayOfMonth(),
+                LocalTime.now().getHour(),LocalTime.now().getMinute(),LocalTime.now().getSecond());
+        long inputEndTime = calendar.getTimeInMillis() ;
+        long inputStartTime = calendar.getTimeInMillis() - 86400000L;
+
+        System.out.println(inputStartTime);
+        System.out.println(inputEndTime);
+        System.out.println(function);
+
+        // create terminal command and execute it ----------------------------------------------------------------------
+        String command = "aws logs start-query --log-group-name \"/aws/lambda/"
+                + function + "\" --start-time " + inputStartTime + " --end-time "
+                + inputEndTime + " --query-string \"fields @timestamp,@message,@type\"";
+        String[] exec = {"/bin/bash", "-c", command};
+        String returnString = "";
+        try {
+            //execute the command
+            Process proc = Runtime.getRuntime().exec(exec);
+
+            // Read the output from the command
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(proc.getInputStream()));
+            System.out.println("Here is the standard output of the command:\n");
+            String s = null;
+
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+                returnString += s;
+            }
+
+            // Read any errors from the attempted command
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(proc.getErrorStream()));
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+
+        } catch (Exception e) {
+            //print exception
+            System.out.println("Exception:\n" + e);
+        }
+        //get the query id from the returnValue
+        return returnString.split("queryId\": \"")[1].split("\"}")[0];
+
+    }
+
+    // getter
+    public ArrayList<String> getFailed() {
+        return this.failed;
+    }
+    public ArrayList<String> getFinished() {
+        return this.finished;
+    }
+    public ArrayList<String> getRunning() {
+        return this.running;
+    }
 }
