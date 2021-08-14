d778db432025c93a217c247749740bfadef663be
Stefan Kotrba <stefan.kotrba@uibk.students.ac.at>
2021-08-14 17:50:57 +0200

    add attributes to object

--- src/main/java/at/enactmentengine/serverless/object/FunctionAttributes.java
+++ src/main/java/at/enactmentengine/serverless/object/FunctionAttributes.java
@@ -1,13 +1,19 @@
 package at.enactmentengine.serverless.object;
 
+
 import at.uibk.dps.afcl.functions.objects.PropertyConstraint;
+import at.uibk.dps.function.FaultToleranceSettings;
+import at.uibk.dps.function.Function;
 
 import java.util.List;
 
 public class FunctionAttributes {
+    private String name;
     private String awsName;
-    private List<PropertyConstraint> constraints;
+    private Function function;
     private int parallelCounter;
+    private int functionInput;
+    private boolean isAsync;
 
     public void increaseCounter(){
         this.parallelCounter++;
@@ -23,16 +29,34 @@ public class FunctionAttributes {
     public int getParallelCounter() {
         return parallelCounter;
     }
-    public List<PropertyConstraint> getConstraints() {
-        return constraints;
+    public Function getFunction() {
+        return function;
     }
-    public void setConstraints(List<PropertyConstraint> constraints) {
-        this.constraints = constraints;
+    public void setFunction(Function function) {
+        this.function = function;
     }
     public String getAwsName() {
         return awsName;
     }
     public void setAwsName(String awsName) {
         this.awsName = awsName;
+    }
+    public int getFunctionInput() {
+        return functionInput;
+    }
+    public void setFunctionInput(int functionInput) {
+        this.functionInput = functionInput;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAsync(boolean async) {
+        isAsync = async;
+    }
+    public boolean isAsync() {
+        return isAsync;
     }
 }
