2f88643f37daf252fb938abcbf719c00767e5ec4
Andreas Reheis <Andreas.Reheis@Student.uibk.ac.at>
2022-02-18 22:30:53 +0100

    added example FunctionToFunction

---
+++ examples/testWorkflows/FunctionToFunction/FunctionToFunctionf2.py
@@ -0,0 +1,4 @@
+def lambda_handler(event, context):
+    return {
+        'f2out1': 'This is a Dummy function'
+    }
---
+++ examples/testWorkflows/FunctionToFunction/FunctionToFunctionf3.py
@@ -0,0 +1,4 @@
+def lambda_handler(event, context):
+    return {
+        'f3out1': 'Finished successfully'
+    }
