020c698e3914a3ff7f9443c4290eeb27451880a3
Andreas Reheis <Andreas.Reheis@Student.uibk.ac.at>
2022-02-20 21:23:49 +0100

    add missing import statement

--- src/main/java/at/enactmentengine/serverless/nodes/ExecutableWorkflow.java
+++ src/main/java/at/enactmentengine/serverless/nodes/ExecutableWorkflow.java
@@ -8,6 +8,7 @@ import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
